4b8beaf6a5f3249b467811761d18b6d73dfa52f7
galdreiman <gal.dreiman@gmail.com>
2024-10-01 12:27:03 +0300

    update fake data and type

--- src/types/pages/flexxTableType.ts
+++ src/types/pages/flexxTableType.ts
@@ -1,13 +1,10 @@
 export type FlexxTableType = {
   id: number
-  avatar: string
-  fullName: string
-  post: string
-  email: string
-  city: string
-  start_date: string
-  salary: number
-  age: number
-  experience: string
-  status: number
+  transaction_id: string
+  policy_holder: string
+  amount: number
+  method: string
+  status: string
+  upcoming_task: string
+  overdue_task: string
 }
