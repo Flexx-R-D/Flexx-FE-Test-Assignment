e561b7bad104aa05f9f041c846391242b9c4262b
galdreiman <gal.dreiman@gmail.com>
2024-10-01 11:54:55 +0300

    assignment commit

---
+++ src/types/pages/flexxTableType.ts
@@ -0,0 +1,13 @@
+export type FlexxTableType = {
+  id: number
+  avatar: string
+  fullName: string
+  post: string
+  email: string
+  city: string
+  start_date: string
+  salary: number
+  age: number
+  experience: string
+  status: number
+}
